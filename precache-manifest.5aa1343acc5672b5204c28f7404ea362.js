2fa9c2407b3d13a7dea12380f55b7cfb138a9273
Bivin Pachatt <v-bipach@microsoft.com>
2019-01-21 16:01:54 -0800

    bits added

---
+++ precache-manifest.5aa1343acc5672b5204c28f7404ea362.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "88f72b0b0ae13e517f21",
+    "url": "/static/js/main.88f72b0b.chunk.js"
+  },
+  {
+    "revision": "270cb27e6149ca86191f",
+    "url": "/static/js/1.270cb27e.chunk.js"
+  },
+  {
+    "revision": "88f72b0b0ae13e517f21",
+    "url": "/static/css/main.4ffecfa3.chunk.css"
+  },
+  {
+    "revision": "c743b307ad87b7fab899c01a0711f087",
+    "url": "/index.html"
+  }
+];
